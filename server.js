26f93b4307a56440fd4e9fdc3ff9677f5d7b27cb
David Hong <phazex1@gmail.com>
2021-10-26 16:11:09 -0400

    Adding rollbar

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require('express')
 const path = require('path')
-// const Rollbar = require('rollbar')
+const Rollbar = require('rollbar')
+
+let rollbar = new Rollbar ({
+    accessToken: '8ac023742d2b424d91f206c06db3e12e',
+    captureUncaught: true,
+    captureUnhandledRejections: true
+})
 
 const app = express()
 app.use(express.json())
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
+    rollbar.info('html file served successfully')
 })
+
+app.use(rollbar.errorHandler())
 
 const port = process.env.PORT || 4545
 
